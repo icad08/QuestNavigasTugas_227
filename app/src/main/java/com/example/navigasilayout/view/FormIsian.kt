ff9a89ef5bbb947069691c817e5221ac650e679f
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:56:54 +0700

    add text jenis kelamin, column, box beserta propertinya

--- app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
+++ app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
@@ -3,13 +3,16 @@ package com.example.navigasilayout.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -72,3 +75,19 @@ fun FormIsian(
             )
 
             Spacer(modifier = Modifier.height(20.dp))
+
+            Text(text = "Jenis Kelamin", fontSize = 12.sp, color = Color.Gray)
+            Column {
+                listJenisKelamin.forEach { item ->
+                    Row(
+                        modifier = Modifier.selectable(
+                            selected = textJK == item,
+                            onClick = { textJK = item }
+                        ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(selected = textJK == item, onClick = { textJK = item })
+                        Text(text = item)
+                    }
+                }
+            }
