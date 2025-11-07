349e5e3c2c1dc892cf088f444f4d2991afd92b84
icad08 <irsyadrm08@gmail.com>
2025-11-07 17:39:55 +0700

    add import R

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.navigasilayout.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
