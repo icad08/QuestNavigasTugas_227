b4ec6b43f7d2a274292244360d19c998b60d3508
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:30:01 +0700

    menambahkan properti color pada topbar

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -3,9 +3,11 @@ package com.example.navigasilayout.view
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,3 +32,8 @@ fun TampilData(
                         color = Color.White
                     )
                 },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        })
