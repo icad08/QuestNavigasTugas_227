d20685377a145ec9f6b47c26fcd1ff45476632ee
icad08 <irsyadrm08@gmail.com>
2025-11-07 17:30:31 +0700

    menambahkan column dan memberi warna pada background

--- app/src/main/java/com/example/navigasilayout/view/Home.kt
+++ app/src/main/java/com/example/navigasilayout/view/Home.kt
@@ -4,4 +4,12 @@ package com.example.navigasilayout.view
 fun Home(
     onSubmitClick: () -> Unit,
     modifier: Modifier = Modifier
-)
+){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF3E5F5))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
