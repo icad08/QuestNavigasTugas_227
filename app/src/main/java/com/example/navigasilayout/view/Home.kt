4ad8d7466ad83f30b80d3c44802670e3006b7cd5
icad08 <irsyadrm08@gmail.com>
2025-11-07 17:29:32 +0700

    reset home. make composable dan function home

--- app/src/main/java/com/example/navigasilayout/view/Home.kt
+++ app/src/main/java/com/example/navigasilayout/view/Home.kt
@@ -1,29 +1,7 @@
 package com.example.navigasilayout.view
 
-import android.R
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import java.lang.reflect.Modifier
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-
-fun HomePage(
-    onNavigateToFormIsian: () -> Unit,
+fun Home(
+    onSubmitClick: () -> Unit,
     modifier: Modifier = Modifier
-){
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                title = {Text(text = "Home", color = Color.White)},
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ){
-
-    }
-}
+)
