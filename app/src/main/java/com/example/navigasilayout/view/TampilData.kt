8163de741c0ee3d51ddcb962185513573363adec
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:28:31 +0700

    membuat variable berserta propertinya

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -1,6 +1,7 @@
 package com.example.navigasilayout.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,3 +10,9 @@ fun TampilData(
     OnBackBtnClick: () -> Unit,
     onBackToHomeClick: () -> Unit = {}
 )
+{
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
