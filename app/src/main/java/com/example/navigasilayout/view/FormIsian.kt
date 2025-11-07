ab615b6d8267880c97eaf2b1d6b2c75d688a41d4
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:48:46 +0700

    add variable beserta propertinya

--- app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
+++ app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
@@ -1,10 +1,19 @@
 package com.example.navigasilayout.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     modifier: Modifier = Modifier,
     OnSubmitBtnClick: () -> Unit
-)
+){
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
