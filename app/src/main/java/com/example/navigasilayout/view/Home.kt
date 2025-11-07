5c47bf21494a4aafbae561a25994d0443106608d
icad08 <irsyadrm08@gmail.com>
2025-11-07 15:29:36 +0700

    menambahkan properti pada topbar

--- app/src/main/java/com/example/navigasilayout/view/Home.kt
+++ app/src/main/java/com/example/navigasilayout/view/Home.kt
@@ -1,6 +1,10 @@
 package com.example.navigasilayout.view
 
+import android.R
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import java.lang.reflect.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -11,6 +15,15 @@ fun HomePage(
     modifier: Modifier = Modifier
 ){
     Scaffold (
-        topBar =
-    )
+        topBar = {
+            TopAppBar(
+                title = {Text(text = "Home", color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ){
+
+    }
 }
