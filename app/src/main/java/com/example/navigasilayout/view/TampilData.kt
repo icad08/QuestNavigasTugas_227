905126971ece1a4003a7151fa9f58bfba35ea38b
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:27:29 +0700

    benerin code

--- app/src/main/java/com/example/navigasilayout/view/TampilData.kt
+++ app/src/main/java/com/example/navigasilayout/view/TampilData.kt
@@ -1,2 +1,11 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun TampilData(
+    OnBackBtnClick: () -> Unit,
+    onBackToHomeClick: () -> Unit = {}
+)
