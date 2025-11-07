d4472e72458f64b58a2b79c882bdc9643abdfe3f
icad08 <irsyadrm08@gmail.com>
2025-11-07 16:47:45 +0700

    add composable dan function form isian

--- app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
+++ app/src/main/java/com/example/navigasilayout/view/FormIsian.kt
@@ -1,2 +1,10 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormIsian(
+    modifier: Modifier = Modifier,
+    OnSubmitBtnClick: () -> Unit
+)
