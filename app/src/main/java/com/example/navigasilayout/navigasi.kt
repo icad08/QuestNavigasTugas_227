79e20c8896f782c94df3130fae691a8877ffd3f4
icad08 <irsyadrm08@gmail.com>
2025-11-07 15:38:11 +0700

    add navHost beserta propertinya

--- app/src/main/java/com/example/navigasilayout/navigasi.kt
+++ app/src/main/java/com/example/navigasilayout/navigasi.kt
@@ -1,5 +1,9 @@
 package com.example.navigasilayout
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.navigation.compose.NavHost
+
 enum class Navigasi{
     Formulir,
     Detail
@@ -10,5 +14,11 @@ fun DataApp (
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
-
+    Scaffold{
+            isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = com.example.navigasi.view.Navigasi.Formulir.name,
+            modifier = modifier.padding(isiRuang))
+    }
 }
