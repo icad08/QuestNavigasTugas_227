a56a4d17b9c9b43661e427dd08665b94e61a8df8
icad08 <irsyadrm08@gmail.com>
2025-11-07 15:39:31 +0700

    menambahkan composable dan formisian pada navhost

--- app/src/main/java/com/example/navigasilayout/navigasi.kt
+++ app/src/main/java/com/example/navigasilayout/navigasi.kt
@@ -3,6 +3,7 @@ package com.example.navigasilayout
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 enum class Navigasi{
     Formulir,
@@ -19,6 +20,12 @@ fun DataApp (
         NavHost(
             navController = navController,
             startDestination = com.example.navigasi.view.Navigasi.Formulir.name,
-            modifier = modifier.padding(isiRuang))
-    }
-}
+            modifier = modifier.padding(isiRuang)){
+
+            composable(route = com.example.navigasi.view.Navigasi.Formulir.name){
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(com.example.navigasi.view.Navigasi.Detail.name)
+                    }
+                }
+            }
