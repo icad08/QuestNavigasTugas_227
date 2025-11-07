921a1915a4601a32f21d7b98bc78a710e0db9d42
icad08 <irsyadrm08@gmail.com>
2025-11-07 15:23:25 +0700

    add scaffold dan topbar

--- app/src/main/java/com/example/navigasilayout/view/Home.kt
+++ app/src/main/java/com/example/navigasilayout/view/Home.kt
@@ -1,4 +1,16 @@
 package com.example.navigasilayout.view
 
+import androidx.compose.material3.Scaffold
+import java.lang.reflect.Modifier
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+
+fun HomePage(
+    onNavigateToFormIsian: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar =
+    )
+}
